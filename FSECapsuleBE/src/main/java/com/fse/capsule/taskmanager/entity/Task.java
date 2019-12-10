85d729b235893ef8c113d514381ed0783866a428
O3407 <O3407@DAUHNEC58AF.bankfab.com>
2019-12-10 17:46:55 +0400

    Changes-0.1

--- FSECapsuleBE/src/main/java/com/fse/capsule/taskmanager/entity/Task.java
+++ FSECapsuleBE/src/main/java/com/fse/capsule/taskmanager/entity/Task.java
@@ -1,5 +1,76 @@
 package com.fse.capsule.taskmanager.entity;
 
-public class Task {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TASK_MASTER")
+public class Task implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String taskName;
+	private String parentTaskName;
+	private String startDate;
+	private String endDate;
+	private String priority;
+	private String status;
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getParentTaskName() {
+		return parentTaskName;
+	}
+
+	public void setParentTaskName(String parentTaskName) {
+		this.parentTaskName = parentTaskName;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
